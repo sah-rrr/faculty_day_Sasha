69f31c6548ac8702701134a3718bdbf3c6456740
alexandraromanova <alexandra_a_romanova@mail.ru>
2025-11-06 09:50:07 +0300

    Добавили Hello world

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
@@ -1,11 +1,5 @@
 package ru.tbank.education.school.lesson1
 
 fun main() {
-    val a = 150
-    var b = 66
-    b++
-    val c: Int? = null
-    println("На матинфо $a учеников")
-    println(b)
-    println(c)
+    println("Hello world!")
 }
