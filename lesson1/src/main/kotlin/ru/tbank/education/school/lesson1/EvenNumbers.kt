326959bc2c62db7e54ad5528a25f09d8f67111cd
alexandraromanova <alexandra_a_romanova@mail.ru>
2025-11-13 10:55:47 +0300

    Домашка калькулятор

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
@@ -8,5 +8,5 @@ fun sumEvenNumbers(numbers: Array<Int>) =
 
 fun main() {
     val numbers = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    println(sumEvenNumbers(numbers))
+    print(sumEvenNumbers(numbers))
 }
