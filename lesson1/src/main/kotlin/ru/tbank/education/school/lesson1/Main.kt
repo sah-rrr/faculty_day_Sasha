e54b20caef77b31d809f4de94fe20ad0285bab91
alexandraromanova <alexandra_a_romanova@mail.ru>
2025-11-13 10:55:47 +0300

    Домашка калькулятор

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
@@ -1,5 +1,11 @@
 package ru.tbank.education.school.lesson1
 
 fun main() {
-    println("Hello world!")
+    val a = 150
+    var b = 66
+    b++
+    val c: Int? = null
+    println("На матинфо $a учеников")
+    println(b)
+    println(c)
 }
