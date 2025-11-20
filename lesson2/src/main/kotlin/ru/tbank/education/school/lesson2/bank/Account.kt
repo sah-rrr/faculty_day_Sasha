3603ff0eab18e0471a3661aa6f837a41542f7e5d
alexandraromanova <alexandra_a_romanova@mail.ru>
2025-11-20 10:53:33 +0300

    Дз "Зоопарк"

--- lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/bank/Account.kt
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/bank/Account.kt
@@ -1,6 +1,6 @@
 package ru.tbank.education.school.lesson2.bank
 
-open class Account(
+open class Account (
     val id: String,
     var balance: Double,
     val customerId: String
@@ -22,20 +22,17 @@ class CreditAccount(
     id: String,
     balance: Double,
     customerId: String,
-    creditLimit: Double,
-) : Account(
+): Account(
     id,
     balance,
-    customerId,
 ) {
     var creditLimit = creditLimit
 
     override fun withdraw(amount: Double): Boolean {
-      if (creditLimit + balance >= amount) {
-          balance -= amount
-          return true
-      }
+        if (creditLimit + balance >= amount) {
+            balance -= amount
+            return true
+        }
         return false
     }
 }
-
